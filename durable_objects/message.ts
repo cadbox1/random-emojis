8a8207571660de78a7c40f9fb48d54cd6398a4d3
Cadell Christo <git@cadell.dev>
2022-01-12 08:54:50 +0800

    add ts-ignores

--- durable_objects/message.ts
+++ durable_objects/message.ts
@@ -1,21 +1,29 @@
 export class Message {
     constructor(state) {
+      // @ts-ignore
       this.state = state;
+      // @ts-ignore
       this.state.blockConcurrencyWhile(async () => {
+        // @ts-ignore
         let stored = await this.state.storage.get("value");
+        // @ts-ignore
         this.value = stored || 0;
       });
     }
   
     async fetch(request) {
       const url = new URL(request.url);
+      // @ts-ignore
       let currentValue = this.value;
   
       if (url.pathname === "/increment") {
+        // @ts-ignore
         currentValue = ++this.value;
+        // @ts-ignore
         await this.state.storage.put("value", currentValue);
       }
   
+      // @ts-ignore
       return jsonResponse(currentValue);
     }
   }
